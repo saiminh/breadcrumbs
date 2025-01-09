4cdd4a50d2ca6b4bc9d63e7e6da88963f1a13036
saiminh <saiminh@gmail.com>
2025-01-09 16:34:51 +0100

    description

--- breadcrumbs.php
+++ breadcrumbs.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Breadcrumbs
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       Breadcrumb navigation
  * Requires at least: 6.6
  * Requires PHP:      7.2
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Version:           1.0
+ * Author:            Simon Flöter
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       breadcrumbs
